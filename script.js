30d1f27d7e6371e634f683db6586d877bd809f0a
afsheen1323 <afsheenkausar2310@gmail.com>
2024-09-23 18:23:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+let box = document.getElementById('column1');
+let hover = document.getElementById('main-box1'); 
+box.addEventListener('mouseover',function(){
+    hover.style.display="block";
+    box.style.transition = "all 2s";
+});
+box.addEventListener('mouseout',function(){
+    hover.style.display="none";
+});
+
+let box2 = document.getElementById('column2');
+let hover2 = document.getElementById('main-box2'); 
+box2.addEventListener('mouseover',function(){
+    hover2.style.display="block";
+});
+box2.addEventListener('mouseout',function(){
+    hover2.style.display="none";
+});
+
+let box3 = document.getElementById('column3');
+let hover3 = document.getElementById('main-box3'); 
+box3.addEventListener('mouseover',function(){
+    hover3.style.display="block";
+});
+box3.addEventListener('mouseout',function(){
+    hover3.style.display="none";
+});
+
+let box4 = document.getElementById('column4');
+let hover4 = document.getElementById('main-box4'); 
+box4.addEventListener('mouseover',function(){
+    hover4.style.display="block";
+});
+box4.addEventListener('mouseout',function(){
+    hover4.style.display="none";
+});
+
+let box5 = document.getElementById('column5');
+let hover5 = document.getElementById('main-box5'); 
+box5.addEventListener('mouseover',function(){
+    hover5.style.display="block";
+});
+box5.addEventListener('mouseout',function(){
+    hover5.style.display="none";
+});
+
+let box6 = document.getElementById('column6');
+let hover6 = document.getElementById('main-box6'); 
+box6.addEventListener('mouseover',function(){
+    hover6.style.display="block";
+});
+box6.addEventListener('mouseout',function(){
+    hover6.style.display="none";
+});
+
+
+
+let box7 = document.getElementById('column7');
+let hover7 = document.getElementById('main-box7'); 
+box7.addEventListener('mouseover',function(){
+    hover7.style.display="block";
+    box7.style.transition = "all 2s";
+});
+box7.addEventListener('mouseout',function(){
+    hover7.style.display="none";
+});
+
+let box8 = document.getElementById('column8');
+let hover8 = document.getElementById('main-box8'); 
+box8.addEventListener('mouseover',function(){
+    hover8.style.display="block";
+});
+box8.addEventListener('mouseout',function(){
+    hover8.style.display="none";
+});
+
+let box9 = document.getElementById('column9');
+let hover9 = document.getElementById('main-box9'); 
+box9.addEventListener('mouseover',function(){
+    hover9.style.display="block";
+});
+box9.addEventListener('mouseout',function(){
+    hover9.style.display="none";
+});
+
+let box10 = document.getElementById('column10');
+let hover10= document.getElementById('main-box10'); 
+box10.addEventListener('mouseover',function(){
+    hover10.style.display="block";
+});
+box10.addEventListener('mouseout',function(){
+    hover10.style.display="none";
+});
+
+let box11 = document.getElementById('column11');
+let hover11 = document.getElementById('main-box11'); 
+box11.addEventListener('mouseover',function(){
+    hover11.style.display="block";
+});
+box11.addEventListener('mouseout',function(){
+    hover11.style.display="none";
+});
+
+let box12 = document.getElementById('column12');
+let hover12 = document.getElementById('main-box12'); 
+box12.addEventListener('mouseover',function(){
+    hover12.style.display="block";
+});
+box12.addEventListener('mouseout',function(){
+    hover12.style.display="none";
+});
